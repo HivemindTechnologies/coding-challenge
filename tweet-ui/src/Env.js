60725a8c13d005826e17063ea3bd1028e72ed61f
Jan Schulte <jan.schulte@hivemindtechnologies.com>
2020-12-02 07:44:20 +0000

    Improve readme.

--- tweet-ui/src/Env.js
+++ tweet-ui/src/Env.js
@@ -1,6 +1,6 @@
 "use strict"
 
 exports.baseUrlEnv = () => {
-    console.log("BASE_URL in JavaScript:\n"+process.env.BASE_URL)
+    console.log("BASE_URL in JavaScript: "+process.env.BASE_URL)
     return process.env.BASE_URL
 }
